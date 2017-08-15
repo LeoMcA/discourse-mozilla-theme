fe76095747e27da394c390c9673d1ec0dbecd2b5
Leo McArdle <leomcardle@gmail.com>
2017-08-15 08:36:06 +0100

    removed log in sign up code (so it can be put in discourse-mozilla-iam plugin)

--- src/common/plugin.js
+++
@@ -1,9 +0,0 @@
-api.reopenWidget('header-buttons', {
-  html(attrs) {
-    if (this.currentUser) { return }
-    return this.attach('button', { contents: api.h('span.d-button-label', I18n.t('log_in') + " / " + I18n.t('sign_up')),
-                                   className: 'btn-primary btn-small login-button',
-                                   action: 'showLogin',
-                                   icon: 'user' })
-  }
-})
